e6526b96421c53cf8f980150a46af0ae71283502
Bob Pakhriev <boburpakhriev@gmail.com>
2021-07-15 17:20:46 +0900

    Initial commit" q ` `
    w
    cd
    `
    
    m
    "

---
+++ src/components/ExpenseItem.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function ExpenseItem() {
+  return (
+    <div>
+      <div>March 28, 2021</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$292.56</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
